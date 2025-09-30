c964bef37511e18cf128ad3562f8b15d0c1ad9cc
Yoann BAUMERT <baumert.yoann@gmail.com>
2025-09-30 19:33:36 +0200

    adding tamed feature for zombiehorses

--- src/main/java/arthessia/featurebox/unused/Unused.java
+++ src/main/java/arthessia/featurebox/unused/Unused.java
@@ -4,10 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Illusioner;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Rabbit;
 import org.bukkit.entity.ZombieHorse;
 import org.bukkit.entity.Rabbit.Type;
@@ -16,6 +20,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import arthessia.featurebox.Plugin;
 import lombok.RequiredArgsConstructor;
@@ -24,15 +30,6 @@ import lombok.RequiredArgsConstructor;
 public class Unused implements Listener {
 
     private final Plugin plugin;
-
-    @EventHandler
-    public void onEntityInteract(PlayerInteractEntityEvent event) {
-        if (event.getRightClicked() instanceof ZombieHorse) {
-            ZombieHorse zombieHorse = (ZombieHorse) event.getRightClicked();
-            zombieHorse.setTamed(true);
-            zombieHorse.setOwner(event.getPlayer());
-        }
-    }
 
     @EventHandler
     public void updateChunckEntities(ChunkLoadEvent event) {
@@ -78,11 +75,67 @@ public class Unused implements Listener {
                     zombieHorse.setDomestication(horse.getDomestication());
                     zombieHorse.setBreed(horse.canBreed());
                     zombieHorse.setTamed(horse.isTamed());
+                    if (horse.isTamed() && horse.getOwner() != null) {
+                        zombieHorse.setOwner(horse.getOwner());
+                    }
 
                     // Optionnel : Copier d'autres attributs selon les besoins
                     zombieHorse.setJumpStrength(((Horse) horse).getJumpStrength());
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        if (!(event.getRightClicked() instanceof ZombieHorse zombieHorse))
+            return;
+
+        Player player = event.getPlayer();
+        ItemStack hand = player.getInventory().getItemInMainHand();
+
+        if (!zombieHorse.isTamed()) {
+            if (hand.getType() == Material.ROTTEN_FLESH) {
+                int randomInt = Plugin.RANDOM.nextInt(100);
+                int chance = plugin.getConfig().getInt("unused.zombiehorse.taming.chance", 33);
+
+                if (randomInt <= chance) {
+                    zombieHorse.setTamed(true);
+                    zombieHorse.setOwner(player);
+                    zombieHorse.getWorld().playSound(zombieHorse.getLocation(), Sound.ENTITY_HORSE_BREATHE, 1f, 1f);
+                    zombieHorse.getWorld().spawnParticle(Particle.HEART, zombieHorse.getLocation().add(0, 1, 0), 5);
+                } else {
+                    zombieHorse.getWorld().playSound(zombieHorse.getLocation(), Sound.ENTITY_HORSE_EAT, 1f, 1f);
+                }
+
+                hand.setAmount(hand.getAmount() - 1);
+            } else if (hand.getType() == Material.SADDLE) {
+                zombieHorse.getWorld().playSound(zombieHorse.getLocation(), Sound.ENTITY_HORSE_ANGRY, 1f, 1f);
+                zombieHorse.getWorld().spawnParticle(Particle.ANGRY_VILLAGER, zombieHorse.getLocation().add(0, 1, 0), 3,
+                        0.2, 0.5, 0.2, 0.01);
+                zombieHorse.setVelocity(zombieHorse.getVelocity().add(new Vector(0, 0.2, 0)));
+            } else {
+                zombieHorse.getWorld().playSound(zombieHorse.getLocation(), Sound.ENTITY_HORSE_ANGRY, 1f, 1f);
+                zombieHorse.getWorld().spawnParticle(Particle.ANGRY_VILLAGER, zombieHorse.getLocation().add(0, 1, 0), 3,
+                        0.2, 0.5, 0.2, 0.01);
+                zombieHorse.setVelocity(zombieHorse.getVelocity().add(new Vector(0, 0.2, 0)));
+            }
+
+            event.setCancelled(true);
+            return;
+        }
+
+        ItemStack saddle = zombieHorse.getInventory().getItem(0); // slot 0 = selle
+        if ((saddle == null || saddle.getType() != Material.SADDLE) && hand.getType() == Material.SADDLE) {
+            zombieHorse.getInventory().setItem(0, new ItemStack(Material.SADDLE));
+            hand.setAmount(hand.getAmount() - 1);
+            event.setCancelled(true);
+            return;
+        }
+
+        if (saddle != null && saddle.getType() == Material.SADDLE) {
+            zombieHorse.addPassenger(player);
+            event.setCancelled(true);
         }
     }
 
